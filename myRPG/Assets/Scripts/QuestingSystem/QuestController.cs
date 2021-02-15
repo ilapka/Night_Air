091093464f7386e88cd320859e5655d38aa244d2
Ivan <78743618+ilapka@users.noreply.github.com>
2021-02-15 14:21:23 +0300

    Update QuestController.cs

--- myRPG/Assets/Scripts/QuestingSystem/QuestController.cs
+++ myRPG/Assets/Scripts/QuestingSystem/QuestController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class QuestController : MonoBehaviour
 {
     public static QuestController Instance;
-    public Quest CurrentQuest;
+    public Quest currentQuest;
     private Queue<Quest> questsRepository = new Queue<Quest>();
     private float delayNextQuestSet;
     [SerializeField] GameObject questsContener;
